2bf877d89ead6de71f944df03681a13730a53ad1
sailendra018 <sailendra018@gmail.com>
2025-08-09 23:28:16 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,15 @@
+# Perform Basic Mathematical Operators
+
+a=input('Enter the First Number: ')
+b=input('Enter the Second Number: ')
+a=float(a)
+b=float(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print('\n ')
+print('Addtion: ',c)
+print('Subtraction: ',d)
+print('Multiplication: ',e)
+print('Division: ',f)
---
+++ Task-2.py
@@ -0,0 +1,11 @@
+# Create a personalized Greeting
+
+a=input('Enter your First Name: ')
+b=input('Enter your Last Name: ')
+c="Hello, "
+d=a+" "+b+"! "
+e="Welcome to the Python Program."
+f=c+d+e
+print('\n ')
+print(f)
+
